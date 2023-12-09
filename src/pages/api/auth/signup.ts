60c79149ad87e2c2d30c4c84c492dd98ff96b282
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-12-09 22:13:52 +0800

    added user register functionality

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,6 +1,29 @@
+import pool from "@/database/db";
 import type { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  const { username, email, password } = req.body;
+  const created_on = new Date().toLocaleDateString("en-CA");
+  const modified_at = new Date().toLocaleDateString("en-CA");
+  const hashedPassword = await bcrypt.hash(password, 12);
+  // need to solve later issue of checking if code is modified before password is rehashed
+
+  const client = await pool.connect();
+  await client.query(
+    "INSERT INTO users(username, password, email, created_on, modified_at) VALUES ($1, $2, $3, $4, $5)",
+    [username, hashedPassword, email, created_on, modified_at]
+  );
+  const result = await client.query(
+    "SELECT * FROM users ORDER BY id desc LIMIT 1"
+  );
+  client.release();
+
+  res.status(201).json({
+    status: "success",
+    data: { user: result.rows[0] },
+  });
+}
--- src/pages/riding-essentials/index.tsx
+++ src/pages/riding-essentials/index.tsx
@@ -1,7 +1,7 @@
 import CollectionLayout from "@/components/layout/collectionLayout";
 
 export default function Index() {
-  return <div className="">Come see collection of riding-essentials</div>;
+  return <div className="">Come see collection of riding-essentials </div>;
 }
 
 Index.PageLayout = CollectionLayout;
