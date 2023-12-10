68659562c3b518958709d2d5ee021e382ed73fea
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-12-10 09:44:26 +0800

    added password match and min 6 characters checking mechanism

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -6,24 +6,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { username, email, password } = req.body;
+  const { username, email, password, passwordConfirm } = req.body;
   const created_on = new Date().toLocaleDateString("en-CA");
   const modified_at = new Date().toLocaleDateString("en-CA");
-  const hashedPassword = await bcrypt.hash(password, 12);
-  // need to solve later issue of checking if code is modified before password is rehashed
+  try {
+    if (passwordConfirm !== password)
+      throw new Error("Your passwords do not match!");
 
-  const client = await pool.connect();
-  await client.query(
-    "INSERT INTO users(username, password, email, created_on, modified_at) VALUES ($1, $2, $3, $4, $5)",
-    [username, hashedPassword, email, created_on, modified_at]
-  );
-  const result = await client.query(
-    "SELECT * FROM users ORDER BY id desc LIMIT 1"
-  );
-  client.release();
+    if (password.length < 6)
+      throw new Error("Your password needs to be min 6 characters");
 
-  res.status(201).json({
-    status: "success",
-    data: { user: result.rows[0] },
-  });
+    const hashedPassword = await bcrypt.hash(password, 12);
+    // need to solve later issue of checking if code is modified before password is rehashed
+
+    const client = await pool.connect();
+    await client.query(
+      "INSERT INTO users(username, password, email, created_on, modified_at) VALUES ($1, $2, $3, $4, $5)",
+      [username, hashedPassword, email, created_on, modified_at]
+    );
+    const result = await client.query(
+      "SELECT * FROM users ORDER BY id desc LIMIT 1"
+    );
+    client.release();
+
+    res.status(201).json({
+      status: "success",
+      data: { user: result.rows[0] },
+    });
+  } catch (err: any) {
+    res.status(401).json({
+      status: "error",
+      message: err.message,
+    });
+  }
 }
