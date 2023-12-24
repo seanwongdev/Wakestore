07bb58a89bc9f0fa085bb018f0d22f01b4f5beea
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-12-24 18:46:51 +0800

    added animation for accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Category } from "./layout/CollectionLayout";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
+import styles from "./Accordion.module.css"; // Import your CSS module for styling
+import { Category } from "./layout/CollectionLayout";
 
 interface AccordionProps {
   data: Category[];
@@ -8,18 +9,33 @@ interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = ({ data, collection }) => {
+  const [contentHeight, setContentHeight] = useState<number | null>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Measure the content height when it becomes visible
+    if (contentRef.current) {
+      setContentHeight(contentRef.current.scrollHeight);
+    }
+  }, [contentRef]);
+
   const categories = data.filter((item) => item.collection_name === collection);
 
   return (
-    <div className="flex flex-col gap-3 px-5 py-3">
-      {categories.map((item) => (
-        <Link
-          key={item.category_id}
-          href={item.collection_url + item.category_url}
-        >
-          {item.category_name}
-        </Link>
-      ))}
+    <div
+      className={styles.accordionContent}
+      style={{ maxHeight: contentHeight || "0px" }}
+    >
+      <div ref={contentRef} className="flex flex-col gap-3 px-5 py-3">
+        {categories.map((item) => (
+          <Link
+            key={item.category_id}
+            href={item.collection_url + item.category_url}
+          >
+            {item.category_name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
