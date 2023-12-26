f7150c411c95cd75cd31c907c75b4668e33a360e
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-12-26 11:37:19 +0800

    fixed layout for account page

--- src/components/layout/ProfileLayout.tsx
+++ src/components/layout/ProfileLayout.tsx
@@ -5,7 +5,7 @@ import SideProfileNavBar from "../Navbar/SideProfileBar";
 const ProfileLayout: React.FC<LayoutProps> = ({ children }) => {
   const [user, setUser] = useState("");
   return (
-    <div className="flex  h-screen w-[91vw] mx-auto mt-10">
+    <div className="flex  h-auto w-[91vw] mx-auto mt-10">
       <div className="w-[280px]">
         <SideProfileNavBar />
       </div>
